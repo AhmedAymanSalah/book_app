af8a5aac05f1640f28600acde192e8f13b984381
AhmedAymanSalah <aa3901@fayoum.edu.eg>
2024-05-07 19:06:18 +0300

    imple fetchfeaturedBook metod

--- lib/Features/home/data/data_source/home_remote_data_source.dart
+++ lib/Features/home/data/data_source/home_remote_data_source.dart
@@ -1,16 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:bookly/Features/home/data/model/book_model/book_model/book_model.dart';
 import 'package:bookly/Features/home/domain/book_entity.dart';
+import 'package:bookly/core/utils/api_services.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBook();
   Future<List<BookEntity>> fetchNewesdBook();
 }
 
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiServices apiServices;
+  HomeRemoteDataSourceImpl(this.apiServices);
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
   @override
-  Future<List<BookEntity>> fetchFeaturedBook() {
-    // TODO: implement fetchFeaturedBook
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBook() async {
+    var data = await apiServices.get(endPoint: "volumes?q=programming");
+
+    List<BookEntity> books = getBoolList(data);
+
+    return books;
+  }
+
+  List<BookEntity> getBoolList(Map<String, dynamic> data) {
+    
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
@@ -18,5 +35,22 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
     // TODO: implement fetchNewesdBook
     throw UnimplementedError();
   }
-
 }
+
+
+// 
+// class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+//   final ApiServices apiServices;
+//   HomeRemoteDataSourceImpl(this.apiServices);
+
+//   @override
+//   Future<List<BookEntity>> fetchFeaturedBook() async {
+//     var data = await apiServices.get(endPoint: "volumes?q=programming");
+ // **************** extarct metod to getbookList ******************/// 
+//     List<BookEntity> books = [];
+//     for (var bookMap in data['items']) {
+//       books.add(BookModel.fromJson(bookMap));
+//     }
+
+//     return books;
+//   }
