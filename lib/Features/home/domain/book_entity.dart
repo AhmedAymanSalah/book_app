a41178d5d26e376357e76baf5a941694a62377ec
AhmedAymanSalah <aa3901@fayoum.edu.eg>
2024-05-07 18:35:56 +0300

    ApiServices

--- lib/Features/home/domain/book_entity.dart
+++ lib/Features/home/domain/book_entity.dart
@@ -1,9 +1,16 @@
 class BookEntity {
+  final String bookId;
   final String image;
   final String title;
   final String authorName;
   final num rating;
   final num price;
 
-  BookEntity({required this.image, required this.title, required this.authorName, required this.rating, required this.price});
+  BookEntity(
+      {required this.bookId,
+      required this.image,
+      required this.title,
+      required this.authorName,
+      required this.rating,
+      required this.price});
 }
