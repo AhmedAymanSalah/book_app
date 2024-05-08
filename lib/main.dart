0248f9f13f719e0d70ea910e8d0c56e02fff0bbc
AhmedAymanSalah <aa3901@fayoum.edu.eg>
2024-05-08 16:20:21 +0300

    my bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:bookly/Features/home/data/data_source/home_local_data_source.dart';
-import 'package:bookly/Features/home/data/data_source/home_remote_data_source.dart';
 import 'package:bookly/Features/home/data/repos/home_repo_imple.dart';
 import 'package:bookly/Features/home/domain/use_case/fetch_newsed_book_use_case.dart';
 import 'package:bookly/Features/home/presentation/manager/cubit/feature_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/manager/newest_cubit/cubit/newest_book_cubit.dart';
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/api_services.dart';
 import 'package:bookly/core/utils/app_router.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,6 +12,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'Features/home/domain/entities/book_entity.dart';
 import 'Features/home/domain/use_case/fetch_feature_book_use_case.dart';
 import 'core/utils/setup_service_locator.dart';
+import 'core/utils/simple_bloc_observer.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -23,7 +20,7 @@ void main() async {
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewesdBox);
-
+  Bloc.observer = MyBlocObserver();
   runApp(const Bookly());
 }
 
